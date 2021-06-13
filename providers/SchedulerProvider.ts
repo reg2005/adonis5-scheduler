22789866cbf5519e5e9fb6b084babea3c0c9b1ee
Evgeniy <cmp08@ya.ru>
2021-06-13 18:58:07 +0300

    feat: upgrade

--- providers/SchedulerProvider.ts
+++ providers/SchedulerProvider.ts
@@ -8,14 +8,16 @@ import Logger from '@ioc:Adonis/Core/Logger'
  * Scheduler provider
  */
 export default class SchedulerProvider {
+	public static needsApplication = true
 	constructor(protected app: ApplicationContract) {}
 
-	public register() {
+	public async register(): Promise<void> {
 		this.app.container.singleton('Adonis/Addons/Scheduler', () => {
-			const app: Application = this.container.use('Adonis/Core/Application')
-			const logger: typeof Logger = this.container.use('Adonis/Core/Logger')
-			return new Scheudler(app.appRoot, this.container, logger)
+			const app: Application = this.app.container.use('Adonis/Core/Application')
+			const logger: typeof Logger = this.app.container.use('Adonis/Core/Logger')
+			return new Scheudler(app.appRoot, this.app.container, logger)
 		})
-		this.container.bind('Adonis/Addons/Scheduler/Task', () => BaseTask)
+		this.app.container.bind('Adonis/Addons/Scheduler/Task', () => BaseTask)
 	}
+	public async boot(): Promise<void> {}
 }
--- test/scheduler.spec.ts
+++ test/scheduler.spec.ts
@@ -1,12 +1,13 @@
-import { join } from 'path'
+// import { join } from 'path'
 import test from 'japa'
-import { Filesystem } from '@poppinss/dev-utils'
-import { Ioc } from '@adonisjs/fold/build/index'
-import { Application } from '@adonisjs/application/build/standalone'
+// import { Filesystem } from '@poppinss/dev-utils'
+// import { Ioc } from '@adonisjs/fold/build/index'
+import AdonisApplication from 'adonis-provider-tester'
+// import { Application } from '@adonisjs/application/build/standalone'
 import Scheduler from '../src/Scheduler'
 import SchedulerProvider from '../providers/SchedulerProvider'
 import { BaseTask } from '../index'
-import { Logger } from '@adonisjs/logger/build/standalone'
+// import { Logger } from '@adonisjs/logger/build/standalone'
 import { LoggerConfig } from '@ioc:Adonis/Core/Logger'
 
 export const loggerConfig: LoggerConfig = {
@@ -16,22 +17,32 @@ export const loggerConfig: LoggerConfig = {
 	prettyPrint: true,
 }
 
-const fs = new Filesystem(join(__dirname, '__app'))
-test.group('Scheduler', () => {
-	function getApp() {
-		console.log('fs.basePath', fs.basePath)
-		const ioc = new Ioc()
-		ioc.bind('Adonis/Core/Application', () => new Application(join(fs.basePath, 'build'), {} as any, {} as any, {}))
-		ioc.bind('Adonis/Core/Logger', () => new Logger(loggerConfig))
-		const schedulerProvider = new SchedulerProvider(ioc)
-		schedulerProvider.register()
-		const scheduler: Scheduler = ioc.use('Adonis/Addons/Scheduler')
-		return { scheduler }
-	}
-	let isHandled = false
+// const fs = new Filesystem(join(__dirname, '__app'))
+test.group('Scheduler', (group) => {
+	let adonisApp: AdonisApplication
+
+	group.before(async () => {
+		adonisApp = await AdonisApplication.initApplication([SchedulerProvider])
+	})
+
+	group.after(async () => {
+		await adonisApp.stopServer()
+	})
+
+	// function getApp() {
+	// console.log('fs.basePath', fs.basePath)
+	// ioc.bind('Adonis/Core/Application', () => new Application(join(fs.basePath, 'build'), {} as any, {} as any, {}))
+	// ioc.bind('Adonis/Core/Logger', () => new Logger(loggerConfig))
+	// const schedulerProvider = new SchedulerProvider(ioc)
+	// schedulerProvider.register()
+	// const scheduler: Scheduler = ioc.use('Adonis/Addons/Scheduler')
+	// return { scheduler }
+	// }
+	let withLockCounter = 0
+	let withoutLockCounter = 0
 	test('Should run with good tasks', async (assert) => {
-		const { scheduler } = getApp()
-		class GoodTask extends BaseTask {
+		const scheduler: Scheduler = adonisApp.application.container.use('Adonis/Addons/Scheduler')
+		class GoodTaskWithLock extends BaseTask {
 			public static get schedule() {
 				return '* * * * * *'
 			}
@@ -42,17 +53,36 @@ test.group('Scheduler', () => {
 
 			public async handle() {
 				const ms = new Date().getTime()
-				console.log('handle start', ms)
+				console.log(this.name, 'handle start', ms)
 				await new Promise((resolve) => setTimeout(resolve, 3000))
-				isHandled = true
-				console.log('handle end', ms)
+				withLockCounter++
+				console.log(this.name, 'handle end', ms)
 			}
 		}
-		await scheduler.run([GoodTask])
+		class GoodTaskWithoutLock extends BaseTask {
+			public static get schedule() {
+				return '* * * * * *'
+			}
+
+			public static get useLock() {
+				return false
+			}
+
+			public async handle() {
+				const ms = new Date().getTime()
+				console.log(this.name, 'handle start', ms)
+				await new Promise((resolve) => setTimeout(resolve, 3000))
+				withoutLockCounter++
+				console.log(this.name, 'handle end', ms)
+			}
+		}
+		await scheduler.run([GoodTaskWithLock, GoodTaskWithoutLock])
 		assert.isArray(scheduler.getRegisteredTasks())
-		assert.equal(scheduler.getRegisteredTasks().length, 1)
+		assert.equal(scheduler.getRegisteredTasks().length, 2)
 
 		await new Promise((resolve) => setTimeout(resolve, 10000))
-		assert.equal(isHandled, true)
+		console.log({ withoutLockCounter, withLockCounter })
+		assert.isTrue(withLockCounter < 5)
+		assert.isTrue(withoutLockCounter > 5)
 	}).timeout(15000)
 })
